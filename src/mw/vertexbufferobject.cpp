13bc69d217da0c724308d144a22fcbde8e69e391
mwthinker <mwthinker@yahoo.se>
2014-09-21 01:21:18 +0200

    Simple help class to create VBO.

---
+++ src/mw/vertexbufferobject.cpp
@@ -0,0 +1,41 @@
+#if MW_OPENGLES2
+#include "vertexbufferobject.h"
+#include "opengl.h"
+
+namespace mw {
+
+	VertexBufferObject::VertexBufferObject() : vboId_(0), target_(0), size_(0), data_(std::make_shared<Data>()) {
+	}
+
+	void VertexBufferObject::bindBufferData(GLenum target, GLsizeiptr size, const GLvoid* data, GLenum usage) {
+		if (vboId_ == 0) {
+			target_ = target;
+			size_ = size;
+			mw::glGenBuffers(1, &vboId_);
+			mw::glBindBuffer(target, vboId_);
+			mw::glBufferData(target, size, data, usage);
+			mw::glBindBuffer(target_, 0);
+			data_->vboId_ = vboId_;
+		}
+	}
+
+	void VertexBufferObject::bind() {
+		if (vboId_ != 0) {
+			mw::glBindBuffer(target_, vboId_);
+		}
+	}
+
+	void VertexBufferObject::unbind() {
+		mw::glBindBuffer(target_, vboId_);
+	}
+
+	VertexBufferObject::Data::Data() : vboId_(0) {
+	}
+
+	VertexBufferObject::Data::~Data() {
+		mw::glDeleteBuffers(1, &vboId_);
+	}
+
+} // Namespace mw.
+
+#endif // MW_OPENGLES2
---
+++ src/mw/vertexbufferobject.h
@@ -0,0 +1,54 @@
+#ifndef VERTEXBUFFEROBJECT_H
+#define VERTEXBUFFEROBJECT_H
+#if MW_OPENGLES2
+
+#include "opengl.h"
+
+#include <memory>
+
+namespace mw {
+
+	class VertexBufferObject {
+	public:
+		// Create a empty vertex buffer object.
+		VertexBufferObject();
+
+		// Generates an id and bind the buffer data to the graphic card. The buffer is then unbind.
+		// Will only bind for the first call to this function.
+		void bindBufferData(GLenum target, GLsizeiptr size, const GLvoid* data, GLenum usage);
+
+		// Bind the current vertex buffer with the initial target.
+		void bind();
+
+		// Unbinds the current vertex buffer.
+		void unbind();
+
+		// Return the size for the current data.
+		inline int getSize() {
+			return size_;
+		}
+
+		// Return the target specified.
+		inline GLenum getTarget() const {
+			return target_;
+		}
+
+	private:
+		class Data {
+		public:
+			Data();
+			~Data();
+
+			GLuint vboId_;
+		};
+
+		GLuint vboId_;
+		GLenum target_;
+		int size_;
+		std::shared_ptr<Data> data_;
+	};
+
+} // Namespace mw.
+
+#endif // MW_OPENGLES2
+#endif // VERTEXBUFFEROBJECT_H
